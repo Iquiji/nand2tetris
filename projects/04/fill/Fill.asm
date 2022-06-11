72811ddd251d9b6c2a696af010a5dc70e77a346c
Iquiji <yt.failerbot.3000@gmail.com>
2022-06-11 22:06:08 +0200

    Fill.asm passes and Project 4 zip finished

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,94 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// Pseudo Code: 
+// loop{
+//     if key_pressed{
+//         draw black in all pixels
+//     }
+//     else{
+//         draw white in all pixels
+//     }
+// }
+
+(START)
+@KBD
+D=M
+
+@FLUSH_BLACK
+D; JNE // Goto Flush black if any key is pressed
+
+@FLUSH_WHITE 
+0; JMP // Else Goto Flush white
+
+(FLUSH_BLACK)
+
+@SCREEN
+D=A
+@current_in_screen // Load in Screen location variable
+M=D
+
+A=M
+M=-1
+
+(FLUSH_BLACK_LOOP)
+
+// Compare with end of loop
+@24576
+D=A
+@current_in_screen
+D=D-M // Calculate Difference
+@START
+D ; JEQ // Jump to start if Difference is zero, else continue flushing to screen
+
+@current_in_screen
+M = M+1
+
+A=M
+M=-1
+
+@KBD
+D=M
+
+@FLUSH_WHITE
+D; JEQ // Goto Flush black if any key is pressed
+
+@FLUSH_BLACK_LOOP // recur flushing black to screen
+0 ; JMP
+
+
+
+(FLUSH_WHITE)
+
+@SCREEN
+D=A
+@current_in_screen // Load in Screen location variable
+M=D
+
+A=M
+M=0
+
+(FLUSH_WHITE_LOOP)
+
+// Compare with end of loop
+@24576
+D=A
+@current_in_screen
+D=D-M // Calculate Difference
+@START
+D ; JEQ // Jump to start if Difference is zero, else continue flushing to screen
+
+@current_in_screen
+M = M+1
+
+A=M
+M=0
+
+@KBD
+D=M
+
+@FLUSH_BLACK
+D; JNE // Goto Flush black if any key is pressed
+
+@FLUSH_WHITE_LOOP // recur flushing white to screen
+0 ; JMP
